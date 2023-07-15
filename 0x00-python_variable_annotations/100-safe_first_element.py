d327a7a2fde7a75b28c0fc893638eb8b9cab3548
Precious Idubor <preciousidubor@gmail.com>
2023-07-15 08:35:09 -0700

    Complex type annotation

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Duck typing"""
+from typing import Union, Any, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
