c212e065a8b57da98371eb469c2e488c07a16620
Precious Idubor <preciousidubor@gmail.com>
2023-08-11 08:47:45 -0700

    Returned list sorted in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,4 +11,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Waits for a random delay and returns it"""
     res = await asyncio.gather(*(wait_random(max_delay) for x in range(n)))
-    return res
+    return sorted(res)
