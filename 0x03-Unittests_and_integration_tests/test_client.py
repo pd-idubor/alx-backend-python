144da3c26d851529b1c434b2c3824746a8f2d5e2
Precious Idubor <preciousidubor@gmail.com>
2023-09-27 01:06:54 -0700

    Fixtures integration test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,8 +4,9 @@ Unit test
 """
 import unittest
 from unittest.mock import patch, PropertyMock
-from parameterized import parameterized, param
+from parameterized import parameterized, param, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -69,6 +70,34 @@ class TestGithubOrgClient(unittest.TestCase):
         gitOC = GithubOrgClient('test')
         self.assertEqual(gitOC.has_license(
             input_payload, expected_license_key), True)
+
+
+@parameterized_class(['org_payload', 'repos_payload',
+                      'expected_repos', 'apache2_repos'], TEST_PAYLOAD)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Test class that inherits from unittest"""
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Setup method
+        """
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload,
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Teardown method
+        """
+        cls.get_patcher.stop()
 
 
 if __name__ == '__main__':
