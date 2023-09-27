52a51adc9e943acf3ea0b41947dcdbdef5c56384
Precious Idubor <preciousidubor@gmail.com>
2023-09-27 00:10:19 -0700

    Parameterize a unit test with expand

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-unit test for utils.access_nested_map.
+Unit Tests
 """
 
 import unittest
@@ -10,19 +10,31 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''
-    test class that inherits from unittest
-    '''
+    """
+    Test class that inherits from unittest
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 3}}, ("a", "b"), 3),
     ])
     def test_access_nested_map(self, nested_map, path, result):
-        '''
-        test method for access_nested_map
-        '''
+        """
+        Test method for access_nested_map
+        """
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, err_message):
+        """
+        Test method for access_nested_map KeyError
+        """
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(err.exception), err_message)
 
 
 if __name__ == '__main__':
