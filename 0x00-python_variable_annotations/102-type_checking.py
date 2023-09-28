887b27cb980506b6fe3ab493f5ab2e784a6ffc45
Precious Idubor <preciousidubor@gmail.com>
2023-09-28 14:49:28 -0700

    Type check

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type Checking"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    "Type check"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
